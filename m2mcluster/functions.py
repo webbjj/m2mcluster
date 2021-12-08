603b71062fedcf78403165a16d81928c22be66e8
webbjj <webb.jjw@gmail.com>
2021-12-08 10:33:52 -0500

    debug chi2 calculation

--- m2mcluster/functions.py
+++ m2mcluster/functions.py
@@ -565,6 +565,6 @@ def binmaker(x, nbin=10, nsum=False, steptype="linear"):
         return x_lower, x_mid, x_upper, x_hist
 
 def chi2(obs,mod):
-    return np.sum((mod-obs)2./obs)
+    return np.sum(((mod-obs)**2.)/obs)
 
 
