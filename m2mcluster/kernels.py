a9d0202f8afc420e0e32381f91b9029ba9af8941
webbjj <webb.jjw@gmail.com>
2022-02-07 20:29:12 -0500

    add epanechnikov kernel

--- m2mcluster/kernels.py
+++ m2mcluster/kernels.py
@@ -33,7 +33,7 @@ def get_kernel(r,rlower,rmid,rupper,kernel='identifier',ndim=3,**kwargs):
         if np.sum(rindx)!=0:
             rmean=rmid[rindx][0]
             sig=ksigma[rindx][0]
-            K_j=gaussian_kernel(rmean,rmean,sig)
+            K_j=gaussian_kernel(rmid,rmean,sig)
         else:
             K_j=np.zeros(len(rlower))
 
@@ -44,6 +44,24 @@ def get_kernel(r,rlower,rmid,rupper,kernel='identifier',ndim=3,**kwargs):
         else:
             K_j*=1.
 
+    elif kernel == 'epanechnikov':
+
+        h=kwargs.get('h',(rupper[0]-rlower[0]))
+        rindx=((r >= rlower) * (r <= rupper))
+
+        if np.sum(rindx)!=0:
+            x=np.fabs(rmid-r)
+            K_j=epanechnikov_kernel(x,h)
+
+        else:
+            K_j=np.zeros(len(rlower))
+
+        if ndim==3 and norm:
+            K_j=K_j*(1./vol)
+        elif ndim==2 and norm:
+            K_j=K_j*(1./area)
+        else:
+            K_j*=1.
 
     return K_j
 
@@ -54,3 +72,17 @@ def gaussian_kernel(r,rmean,sigma):
 	f=(1./np.sqrt(2*np.pi*sigma2))*np.exp(-0.5*((r-rmean)**2.)/sigma2)
 	
 	return f
+
+def epanechnikov_kernel(x,h):
+
+    f=np.zeros(len(x))
+    xindx=(x>=0) * (x<=h)
+    f[xindx]=(3./(4.*h))*(1.0-(x[xindx]/h)**2.)
+
+    return f
+
+def bovy_epanechnikov_kernel(r,h):
+    out= numpy.zeros_like(r)
+    out[(r >= 0.)*(r <= h)]= 3./4.*(1.-r[(r >= 0.)*(r <= h)]**2./h**2.)/h
+    return out
+
